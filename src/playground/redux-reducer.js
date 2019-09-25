20a483d1e86f2352602ec1fc6c9fa7073086301d
Dragunnai27 <shiroyume005@gmail.com>
2019-09-25 09:50:53 +0700

    Adding filter function

--- src/playground/redux-reducer.js
+++ src/playground/redux-reducer.js
@@ -1,7 +1,9 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
 const demoState = {
   expenses: [{
+    id: 'jk47mit20',
     name: 'Thanh Tam',
     amount: 500000
   }],
@@ -11,16 +13,89 @@ const demoState = {
     startDate: undefined,
     endDate: undefined
   }
-}
+};
+
+// ADD_EXPENSES
+const addExpenses = (
+  {
+    name = '',
+    amount = 0,
+    createAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSES',
+  expenses: {
+    id: uuid(),
+    name,
+    amount,
+    createAt
+  }
+});
+
+// REMOVE_EXPENSES
+const removeExpenses = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSES',
+  id
+});
+
+// EDIT_EXPENSES
+const editExpenses = (id, update) => ({
+  type: 'EDIT_EXPENSES',
+  id,
+  update
+})
+
+// SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+})
+
+// SORT_BY_AMOUNT
+const sortByAmount = () => ({
+  type: 'SORT_BY_AMOUNT'
+})
+// SORT_BY_DATE
+const sortByDate = () => ({
+  type: 'SORT_BY_DATE'
+})
+// SET_START_DATE
+const setStartDate = (date = undefined) => ({
+  type: 'SET_START_DATE',
+  date
+})
+// SET_START_DATE
+const setEndDate = (date = undefined) => ({
+  type: 'SET_END_DATE',
+  date
+})
 
 // EXPENSES REDUCER
 const expensesReducerDefault = [];
 const expensesReducer = (state = expensesReducerDefault, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSES':
+      return [...state, action.expenses];
+
+    case 'REMOVE_EXPENSES':
+      return state.filter(({ id }) => { return id !== action.id });
+
+    case 'EDIT_EXPENSES':
+      return state.map((expenses) => {
+        if (expenses.id === action.id) {
+          return {
+            ...state,
+            ...action.update
+          }
+        } else {
+          return state
+        }
+      })
     default:
       return state;
   };
 };
+
 
 // FILTER REDUCER
 const filterReducerDefault = {
@@ -32,9 +107,30 @@ const filterReducerDefault = {
 
 const filterReducer = (state = filterReducerDefault, action) => {
   switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return { ...state, text: action.text }
+    case 'SORT_BY_DATE':
+      return { ...state, sortBy: 'date' }
+    case 'SORT_BY_AMOUNT':
+      return { ...state, sortBy: 'amount' }
+    case 'SET_START_DATE':
+      return { ...state, startDate: action.date }
+    case 'SET_END_DATE':
+      return { ...state, endDate: action.date }
     default:
       return state
   };
+};
+// GET VISIBLE EXPENSIVE
+const getVisibleExpensive = (expenses, { text, sortBy, startDate, endDate }) => {
+  return expenses.filter((expense) => {
+    const startDateMatch = typeof startDate !== 'number' || expense.createAt > startDate;
+    console.log(startDateMatch);
+    const endDateMatch = typeof endDate !== 'number' || expense.createAt < endDateMatch;
+    const textMacth = true;
+
+    return startDateMatch && endDateMatch && textMacth;
+  })
 };
 
 // STORE CREATION
@@ -45,4 +141,40 @@ const store = createStore(
   })
 );
 
-console.log(store.getState())
+store.subscribe(() => {
+  const state = store.getState();
+  const visibleExpensive = getVisibleExpensive(state.expenses, state.filter);
+  console.log(visibleExpensive);
+});
+
+const A = store.dispatch(addExpenses({ name: 'Breakfast', amount: 150, createAt: 100 }));
+const B = store.dispatch(addExpenses({ name: 'Lunch', amount: 150, createAt: 200 }));
+const C = store.dispatch(addExpenses({ name: 'Coffee', amount: 150, createAt: 300 }));
+
+// store.dispatch(removeExpenses({ id: Quyen.expenses.id }))
+// store.dispatch(removeExpenses({ id: Nang.expenses.id }))
+
+// store.dispatch(editExpenses(Nang.expenses.id, { amount: 200000 }))
+
+// store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter());
+
+// store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
+
+store.dispatch(setStartDate(100));
+// store.dispatch(setStartDate());
+
+// store.dispatch(setEndDate(200));
+// store.dispatch(setEndDate());
+
+// // TESTING
+// const student = {
+//   id: '16DH110238',
+//   name: 'Thanh Tam'
+// }
+// const element = {
+//   class: 'TH1604',
+//   major: 'Information technology'
+// }
+// console.log({ ...student, ...element })
